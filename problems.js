591879b9b0ada2b05bcfca91c0f7a437a0f7bc17
Michael Doty <michaeldoty@outlook.com>
2020-09-22 09:09:57 -0700

    Update problems.js

--- problems.js
+++ problems.js
@@ -1,7 +1,7 @@
-***** longestCommonPrefix *****
+***** longestCommonPrefix : JAVASCRIPT *****
 
 var longestCommonPrefix = function(arr) {
-     //edge case if is the input array is empty
+     //EDGE CASE: if is the input array is empty
      if (arr.length === 0) return '';
      
      //create longest prefix variable and set it to the entire first word
@@ -26,4 +26,33 @@ var longestCommonPrefix = function(arr) {
 
 longestCommonPrefix(["flower","flow","flight"])//"fl"
 
-*****  
+***** longestCommonPrefix : C# *****
+
+public class Solution {
+    public string LongestCommonPrefix(string[] strs) {
+        //EDGE CASE:  input array is 0 => ""
+        if (strs.Length == 0) return "";
+        
+        //create longest prefix variable and set it to the entire first word
+        string prefix = strs[0];
+        
+         //loop through all the strings in the array starting at index 1
+        for (int i = 1; i < strs.Length; i++) 
+            
+            //while the next string isnt a prefix of the word before it
+            while (strs[i].IndexOf(prefix) != 0) {
+                 
+                //remove the last letter off the string and check again // repeat
+                prefix = prefix.Substring(0, prefix.Length -1);
+                 
+                //EDGE CASE: If you pull all letters fo the prefix, no common prefix, return '';
+                if (prefix.Length == 0) return "";
+            }
+         
+        //return the longest prefix
+        return prefix;
+    }
+}
+
+
+
